3c6babd11d8f49f5ec27693a025ff9d5ad6a0e50
ikhsanapriliano <ikhsanapriliano4@gmail.com>
2023-11-22 11:04:52 +0700

    15th

--- server/server.ts
+++ server/server.ts
@@ -102,6 +102,52 @@ app.get("/auth/linkedin/callback", async (req: any, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.get("/auth/linkedin", (req, res) => {
+  return res.redirect(Authorization());
+});
+
+app.get("/auth/linkedin/callback", async (req, res) => {
+  const code = await Redirect(req.query.code);
+  const token = code.access_token;
+  const axiosInstance = axios.create({
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  try {
+    const fetch = await axiosInstance.get("https://api.linkedin.com/v2/userinfo");
+    const result = await fetch.data;
+    const { sub, given_name, family_name, picture } = result;
+    const exist = await User.findOne({ sub: sub });
+    if (exist === null) {
+      const newUser = new User({
+        sub: sub,
+        firstName: given_name,
+        lastName: family_name,
+        profile: picture,
+        status: "member",
+        camp: [],
+      });
+      newUser.save().then(() => {
+        console.log("new account");
+        identity = newUser.sub;
+        res.redirect("https://englishbonfire.netlify.app");
+      });
+    } else {
+      console.log("account found");
+      identity = sub;
+      res.redirect("https://englishbonfire.netlify.app");
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 app.get("/personal", async (req, res) => {
   const user = await User.findOne({ sub: identity });
   res.json(user);
@@ -114,15 +160,25 @@ app.get("/community", async (req, res) => {
 
 app.post("/join", async (req, res) => {
   const id = req.body.id;
-  await User.updateOne({ sub: identity }, { $push: { camp: id } });
-  res.redirect("http://localhost:5173/bivouac/finished");
+  const user = await User.findOne({ sub: identity });
+  const exist = user.camp.includes(id);
+  if (exist === false) {
+    await User.updateOne({ sub: identity }, { $push: { camp: id } });
+    res.redirect("https://englishbonfire.netlify.app/bivouac/finished");
+  } else {
+    res.redirect("https://englishbonfire.netlify.app/unknown");
+  }
+});
+
+app.post("/something", (req, res) => {
+  res.redirect("https://englishbonfire.netlify.app/unknown");
 });
 
 app.get("/logout", (req, res) => {
   identity = "";
-  res.redirect("http://localhost:5173");
+  res.redirect("https://englishbonfire.netlify.app");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`your app is running on port ${port}`);
 });
